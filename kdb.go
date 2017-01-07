abb12b4cc09d8041d77d03d3cbd9e9df67789169
chenhq <hongqing.chen@gmail.com>
2017-01-07 21:03:27 +0800

    just for save

--- kdb.go
+++ kdb.go
@@ -1,10 +1,12 @@
 package kdbutils
 
 import (
-	kdbgo "github.com/sv/kdbgo"
+	kdb "github.com/sv/kdbgo"
 	logger "github.com/alecthomas/log4go"
 	"fmt"
 	"errors"
+	"reflect"
+	"strings"
 )
 
 // ref: http://stackoverflow.com/questions/10210188/instance-new-type-golang
@@ -14,7 +16,7 @@ type Factory_New func() interface{}
 type Kdb struct {
 	host string
 	post int
-	con *kdbgo.KDBConn
+	con  *kdb.KDBConn
 }
 
 func MewKdb(host string, port int) *Kdb {
@@ -25,7 +27,7 @@ func MewKdb(host string, port int) *Kdb {
 func (this *Kdb) Connect() error {
 	logger.Info("connecting to kdb, host: %v, port:%v", this.host, this.post)
 	var err error
-	if this.con, err = kdbgo.DialKDB(this.host, this.post, "kdbgo:kdbgo"); err == nil {
+	if this.con, err = kdb.DialKDB(this.host, this.post, "kdbgo:kdbgo"); err == nil {
 		logger.Info("Connected to kdb successful")
 	} else {
 		logger.Error("Failed to connect to kdb, error: %s", err.Error())
@@ -45,7 +47,7 @@ func (this *Kdb) Disconnect() error {
 	return err
 }
 
-func (this *Kdb) Subscribe(table string, sym []string)  {
+func (this *Kdb) Subscribe(table string, sym []string) {
 	if this.con == nil {
 		this.Connect()
 	}
@@ -53,18 +55,18 @@ func (this *Kdb) Subscribe(table string, sym []string)  {
 	logger.Info("Subscribing Kdb, table: %s, sym: %v, sym_num: %v", table, sym, sym_num)
 	var err error
 	if sym_num == 0 {
-		err = this.con.AsyncCall(".u.sub", &kdbgo.K{-kdbgo.KS, kdbgo.NONE, table},
-			&kdbgo.K{-kdbgo.KS, kdbgo.NONE, ""})
+		err = this.con.AsyncCall(".u.sub", &kdb.K{-kdb.KS, kdb.NONE, table},
+			&kdb.K{-kdb.KS, kdb.NONE, ""})
 	} else {
-		err = this.con.AsyncCall(".u.sub", &kdbgo.K{-kdbgo.KS, kdbgo.NONE, table},
-			&kdbgo.K{kdbgo.KS, kdbgo.NONE, sym})
+		err = this.con.AsyncCall(".u.sub", &kdb.K{-kdb.KS, kdb.NONE, table},
+			&kdb.K{kdb.KS, kdb.NONE, sym})
 	}
 	if err != nil {
 		logger.Error("Failed to subscibe, table: %s, sym; %s", table, sym)
 	}
 }
 
-func (this *Kdb) SubscribedData2Channel(channel chan<-interface{}, table2struct map[string]Factory_New)  {
+func (this *Kdb) SubscribedData2Channel(channel chan <-interface{}, table2struct map[string]Factory_New) {
 	for {
 		// ignore type print output
 		res, _, err := this.con.ReadMessage()
@@ -78,7 +80,7 @@ func (this *Kdb) SubscribedData2Channel(channel chan<-interface{}, table2struct 
 			continue
 		}
 
-		data_list := res.Data.([]*kdbgo.K)
+		data_list := res.Data.([]*kdb.K)
 
 		// confirm function is upd
 		func_name := data_list[0].Data.(string)
@@ -94,7 +96,7 @@ func (this *Kdb) SubscribedData2Channel(channel chan<-interface{}, table2struct 
 		match := false
 		for tab, fn := range table2struct {
 			if table_name == tab {
-  				factory = fn
+				factory = fn
 				match = true
 				break
 			}
@@ -103,12 +105,12 @@ func (this *Kdb) SubscribedData2Channel(channel chan<-interface{}, table2struct 
 			continue
 		}
 
-		table_data := data_list[2].Data.(kdbgo.Table)
+		table_data := data_list[2].Data.(kdb.Table)
 		length := table_data.Data[0].Len()
 		logger.Debug("message's length: %d", length)
 		for i := 0; i < length; i++ {
 			row := factory()
-			err := kdbgo.UnmarshalDict(table_data.Index(i), row)
+			err := kdb.UnmarshalDict(table_data.Index(i), row)
 			if err != nil {
 				fmt.Println("Failed to unmrshall dict ", err)
 				continue
@@ -118,7 +120,7 @@ func (this *Kdb) SubscribedData2Channel(channel chan<-interface{}, table2struct 
 	}
 }
 
-func (this *Kdb) QueryTable(query string, factory Factory_New) ([]interface{},error) {
+func (this *Kdb) QueryNoneKeyedTable(query string, v interface{}) (interface{}, error) {
 	if this.con == nil {
 		logger.Error("Kdb is not connected")
 		return nil, errors.New("kdb is not connected")
@@ -129,13 +131,43 @@ func (this *Kdb) QueryTable(query string, factory Factory_New) ([]interface{},er
 		return nil, errors.New("kdb query error")
 	} else {
 		// parse result
-		table := res.Data.(kdbgo.Table)
+		t := res.Data.(kdb.Table)
+		vv := reflect.ValueOf(v)
+		if vv.Kind() != reflect.Ptr || vv.IsNil() {
+			return nil, errors.New("Invalid target type. Shoult be non null pointer")
+		}
+		vv = reflect.Indirect(vv)
+		for i := 0; i < int(t.Data[0].Len()); i++ {
+			emptyelem := reflect.New(vv.Type().Elem())
+			err := kdb.UnmarshalDict(t.Index(i), emptyelem.Interface())
+			if err != nil {
+				fmt.Println("Failed to unmrshall dict", err)
+				return nil, err
+			}
+			vv = reflect.Append(vv, reflect.Indirect(emptyelem))
+		}
+		return vv.Interface(), nil
+	}
+}
+
+func (this *Kdb) QueryNoneKeydTable2(query string, factory Factory_New) ([]interface{}, error) {
+	if this.con == nil {
+		logger.Error("Kdb is not connected")
+		return nil, errors.New("kdb is not connected")
+	}
+
+	if res, err := this.con.Call(query); err != nil {
+		logger.Error("Kdb query error, query: %v, error: %v", query, err)
+		return nil, errors.New("kdb query error")
+	} else {
+		// parse result
+		table := res.Data.(kdb.Table)
 		length := table.Data[0].Len()
 		table_data := make([]interface{}, length, length)
 
 		for i := 0; i < length; i++ {
 			row := factory()
-			if err := kdbgo.UnmarshalDict(table.Index(i), row); err != nil {
+			if err := kdb.UnmarshalDict(table.Index(i), row); err != nil {
 				return nil, err
 			}
 			table_data[i] = row
@@ -143,3 +175,100 @@ func (this *Kdb) QueryTable(query string, factory Factory_New) ([]interface{},er
 		return table_data, nil
 	}
 }
+
+func (this *Kdb) FuncTable(func_name string, table_name string, data interface{}) (interface{}, error) {
+	table := kdb.Table{}
+	if err, table := MarshalTable(data, table); err == nil {
+		logger.Error("MarshalTable error, Err: %v", err)
+		return nil, errors.New("MarshalTable error")
+	} else {
+		k_tab := &kdb.K{kdb.XT, kdb.NONE, table}
+		if ret, err := this.con.Call(func_name, &kdb.K{-kdb.KS, kdb.NONE, table_name}, k_tab); err == nil {
+			logger.Debug("Execute kdb function successfully, func_name: %v, table_name: %v, return: %v",
+				func_name, table_name, ret)
+			return  ret, nil
+		} else {
+			logger.Debug("Execute kdb function failed, func_name: %v, table_name: %v",
+				func_name, table_name)
+			return nil, errors.New("Execute kdb function failed")
+		}
+	}
+}
+
+func MarshalTable(v interface{}, table kdb.Table) (e1 error, t kdb.Table) {
+	var err error = nil
+	var keys = []string{}
+	var values = []*kdb.K{}
+	vv := reflect.ValueOf(v)
+	aa := reflect.ValueOf(v).Elem()
+	if vv.Kind() != reflect.Ptr || vv.IsNil() {
+		err = errors.New("Invalid target type. Should be non null pointer")
+	}
+	vv = reflect.Indirect(vv)
+
+	for i := 0; i < vv.NumField(); i++ {
+		//		fmt.Println("vv.Field(k)", vv.Field(k))
+		//		fmt.Printf("%s -- %v \n", vv.Type().Field(k).Name, vv.Field(k).Kind().String())
+
+		switch vv.Field(i).Kind() {
+		case reflect.Struct: {
+			typ := vv.Field(i).Type()
+			if vv.Field(i).NumField() == 3 && typ.Field(0).Name == "sec" && typ.Field(1).Name == "nsec" {
+				//m := vv.Field(k).MethodByName("Local")
+				//rets := m.Call([]reflect.Value{})
+				/**
+				var t time.Time = rets[0].Interface().(time.Time)
+				m2 := vv.Field(k).MethodByName("Location")
+				rets2 := m2.Call([]reflect.Value{})
+				var l *time.Location = rets2[0].Interface().(*time.Location)
+				// var timeFloat64 float64 = getNumDate(t, l)
+				**/
+				var timeFloat64 float64 = 1
+				fmt.Println("timeFloat64,", timeFloat64)
+				keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+				var tempk = &kdb.K{kdb.KZ, kdb.NONE, []float64{timeFloat64}}
+				values = append(values, tempk)
+
+			}
+			//fallthrough
+		}
+		case reflect.Int32: {
+			keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+			var tempk = &kdb.K{kdb.KI, kdb.NONE, []int32{aa.Field(i).Interface().(int32)}}
+			values = append(values, tempk)
+		}
+		case reflect.Int64: {
+			keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+			var tempk = &kdb.K{kdb.KI, kdb.NONE, []int32{aa.Field(i).Interface().(int32)}}
+			values = append(values, tempk)
+		}
+		case reflect.Float32: {
+			keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+			var tempk = &kdb.K{kdb.KF, kdb.NONE, []float32{aa.Field(i).Interface().(float32)}}
+			values = append(values, tempk)
+		}
+		case reflect.Float64: {
+			if vv.Field(i).Kind() == reflect.Float64 {
+				keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+				var tempk = &kdb.K{kdb.KF, kdb.NONE, []float64{aa.Field(i).Interface().(float64)}}
+				values = append(values, tempk)
+			}
+		}
+		case reflect.Bool: {
+			keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+			var tempk = &kdb.K{kdb.KF, kdb.NONE, []bool{aa.Field(i).Interface().(bool)}}
+			values = append(values, tempk)
+		}
+		case reflect.String: {
+			keys = append(keys, strings.ToLower(vv.Type().Field(i).Name))
+			var tempk = &kdb.K{kdb.KS, kdb.NONE, []string{aa.Field(i).Interface().(string)}}
+			values = append(values, tempk)
+		}
+		}
+	}
+	table.Columns = keys
+	table.Data = values
+	return err, table
+}
+
+
--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/llmofang/kdbutils/tbls"
 	l4g "github.com/alecthomas/log4go"
 	"time"
+	"reflect"
 )
 
 func main() {
@@ -19,12 +20,6 @@ func main() {
 	test_query_table(kdb)
 	test_subscribe(kdb)
 	time.Sleep(10 * time.Second)
-
-
-	//kdb.Disconnect()
-	//kdb.Connect()
-	// test for query table
-
 
 	for {
 		time.Sleep(100 * time.Second)
@@ -72,13 +67,55 @@ func test_subscribe(kdb *kdbutils.Kdb)  {
 func test_query_table(kdb *kdbutils.Kdb) {
 
 	query := "0!select [-10] from ohlcv"
-	if table_data, err := kdb.QueryTable(query, func() interface{} {
+
+	ohlcv := make([]tbls.Ohlcv, 0)
+	// ohlcv := tbls.Ohlcv{}
+	if result, err :=kdb.QueryNoneKeyedTable(query, &ohlcv); err == nil {
+		res := result.([]tbls.Ohlcv)
+		for i := 0; i < len(res); i++ {
+			ohlcv := res[i]
+			l4g.Debug("sym: %v, min: %v, open: %v, high: %v, low: %v, close: %v",
+				ohlcv.Sym, ohlcv.Minute, ohlcv.Open, ohlcv.High, ohlcv.Low, ohlcv.Close)
+		}
+
+		func(data interface{}) {
+			data_s := reflect.ValueOf(data)
+
+			if data_s.Len() > 0 {
+				num_field := reflect.ValueOf(data_s.Index(0)).NumField()
+				for i := 0; i < num_field; i++ {
+					l4g.Debug(data_s.Index(0).Type().Field(i).Name)
+					l4g.Debug(data_s.Index(0).Field(i).Kind())
+					for j := 0; j < data_s.Len(); j++ {
+						l4g.Debug(data_s.Index(j).Field(i).Interface())
+					}
+				}
+			}
+			/**
+			for i := 0; i < data_s.Len(); i++ {
+				s := data_s.Index(i)
+				vv := reflect.ValueOf(s)
+				for j := 0; j < vv.NumField(); j++ {
+					l4g.Debug(s.Type().Field(j).Name)
+					l4g.Debug(s.Field(j).Kind())
+					l4g.Debug(s.Field(j).Interface())
+				}
+			}
+			**/
+		}(res)
+		// test for update
+	}
+
+	l4g.Debug("===============================================================================================")
+
+	if table_data, err := kdb.QueryNoneKeydTable2(query, func() interface{} {
 		return new(tbls.Ohlcv)
 	}); err == nil {
 		for i := 0; i < len(table_data); i++ {
 			ohlcv := table_data[i].(*tbls.Ohlcv)
-			l4g.Debug("sym: %v, min: %v, open: %v, high: %v, low: %v, close: %v", ohlcv.Sym, ohlcv.Minute, ohlcv.Open, ohlcv.High, ohlcv.Low, ohlcv.Close)
-
+			l4g.Debug("sym: %v, min: %v, open: %v, high: %v, low: %v, close: %v",
+				ohlcv.Sym, ohlcv.Minute, ohlcv.Open, ohlcv.High, ohlcv.Low, ohlcv.Close)
 		}
 	}
+
 }
